7850b7607e189766ac2c3e25f968f76d9ac75739
Squnix <kansxy.squnix@gmail.com>
2025-11-22 18:17:01 +0700

    enable bbr2.ps1

---
+++ Etc/enable bbr2.ps1
@@ -0,0 +1,4 @@
+netsh int tcp set supplemental template=internet congestionprovider=CUBIC
+netsh int tcp set supplemental template=internetcustom congestionprovider=CUBIC
+netsh int tcp set supplemental template=Datacenter congestionprovider=CUBIC
+netsh int tcp set supplemental template=Datacentercustom congestionprovider=CUBIC
